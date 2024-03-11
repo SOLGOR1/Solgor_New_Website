b83298da92dbe643e95d06e2abdaf677015d3a11
SOLGOR1 <160745016+SOLGOR1@users.noreply.github.com>
2024-03-11 01:30:01 +0100

    Update testindex.js

--- pages/testindex.js
+++ pages/testindex.js
@@ -1,15 +1,6 @@
 import config from "@config/config.json";
 import Base from "@layouts/Baseof";
 import ImageFallback from "@layouts/components/ImageFallback";
-import Post from "@layouts/partials/Post";
-import Sidebar from "@layouts/partials/Sidebar";
-import { getListPage, getSinglePage } from "@lib/contentParser";
-import { getTaxonomy } from "@lib/taxonomyParser";
-import dateFormat from "@lib/utils/dateFormat";
-import { sortByDate } from "@lib/utils/sortFunctions";
-import { markdownify } from "@lib/utils/textConverter";
-import Link from "next/link";
-import { FaRegCalendar } from "react-icons/fa";
 import Image from "next/image";
 import { useState, useEffect } from 'react';
 
@@ -42,7 +33,46 @@ const Home = ({
     <Base>
       {/* Banner */}
       <section className="section banner relative pb-0">
-        {/* Your banner section code here */}
+        <ImageFallback
+          className="absolute bottom-0 left-0 z-[-1] w-full"
+          src={"/images/banner-bg-shape.svg"}
+          width={1905}
+          height={295}
+          alt="banner-shape"
+          priority
+        />
+        <div className="container">
+          <div className="row flex-wrap-reverse items-center justify-center lg:flex-row">
+            <div className={banner.image_enable ? "mt-12 text-center lg:mt-0 lg:text-left lg:col-6" : "mt-12 text-center lg:mt-0 lg:text-left lg:col-12"}>
+              <div className="banner-title">
+                {markdownify(banner.title, "h1")}
+                {markdownify(banner.title_small, "span")}
+              </div>
+              {markdownify(banner.content, "p", "mt-4")}
+              {banner.button.enable && (
+                <Link
+                  className="btn btn-primary mt-6"
+                  href={banner.button.link}
+                  rel={banner.button.rel}
+                >
+                  {banner.button.label}
+                </Link>
+              )}
+            </div>
+            {banner.image_enable && (
+              <div className="col-9 lg:col-6">
+                <ImageFallback
+                  className="mx-auto object-contain"
+                  src={banner.image}
+                  width={548}
+                  height={443}
+                  priority={true}
+                  alt="Banner Image"
+                />
+              </div>
+            )}
+          </div>
+        </div>
       </section>
 
       {/* Image links section */}
