0c3ace880121be14b1e4a175d24b11063ff15ed6
SOLGOR1 <160745016+SOLGOR1@users.noreply.github.com>
2024-03-20 01:28:02 +0100

    Update Header_TEST.js

--- layouts/partials/Header_TEST.js
+++ layouts/partials/Header_TEST.js
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
+import ConnectWallet from "./ConnectWallet"; // Import the ConnectWallet component
 
 const Header = () => {
   // distructuring the main menu from menu object
@@ -84,7 +85,6 @@ const Header = () => {
                           >
                             <Link
                               href={child.url}
-                              target={child.target?child.target:"_self"}
                               className={`nav-dropdown-link block ${
                                 router.asPath === child.url && "active"
                               }`}
@@ -99,7 +99,6 @@ const Header = () => {
                     <li className="nav-item">
                       <Link
                         href={menu.url}
-                        target={menu.target?menu.target:"_self"}
                         className={`nav-link block ${
                           router.asPath === menu.url && "active"
                         }`}
@@ -124,25 +123,8 @@ const Header = () => {
           >
             <IoSearch />
           </div>
-          <button
-            onClick={() => setShowMenu(!showMenu)}
-            className="inline-flex h-10 w-10 items-center justify-center rounded-full bg-primary text-white lg:hidden"
-          >
-            {showMenu ? (
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <title>Menu Close</title>
-                <polygon
-                  points="11 9 22 9 22 11 11 11 11 22 9 22 9 11 -2 11 -2 9 9 9 9 -2 11 -2"
-                  transform="rotate(45 10 10)"
-                />
-              </svg>
-            ) : (
-              <svg className="h-4 w-4 fill-current" viewBox="0 0 20 20">
-                <title>Menu Open</title>
-                <path d="M0 3h20v2H0V3z m0 6h20v2H0V9z m0 6h20v2H0V0z" />
-              </svg>
-            )}
-          </button>
+          {/* Connect Wallet Button */}
+          <ConnectWallet />
         </div>
 
         <SearchModal
