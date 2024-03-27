711170ec3eb1777da6521635a48698d3537a04d8
SOLGOR1 <160745016+SOLGOR1@users.noreply.github.com>
2024-03-27 15:25:01 +0100

    Update mint2.js

--- pages/mint2.js
+++ pages/mint2.js
@@ -35,24 +35,62 @@ const Home = ({
     };
 
     loadScript();
+
+    // Clean up function to remove the script and link elements when component unmounts
+    return () => {
+      const script = document.querySelector('script[src="https://storage.googleapis.com/scriptslmt/0.1.3/solana.js"]');
+      const link = document.querySelector('link[href="https://storage.googleapis.com/scriptslmt/0.1.3/solana.css"]');
+      if (script) {
+        script.remove();
+      }
+      if (link) {
+        link.remove();
+      }
+    };
   }, []); // Run only once on component mount
 
   return (
     <Base>
       {/* Banner */}
-      {/* Your banner section code goes here */}
-
-      {/* Script and HTML elements */}
       <section className="section banner relative pb-0">
+        <ImageFallback
+          className="absolute bottom-0 left-0 z-[-1] w-full"
+          src={"/images/banner-bg-shape.svg"}
+          width={1905}
+          height={295}
+          alt="banner-shape"
+          priority
+        />
         <div className="container">
           <div className="row flex-wrap-reverse items-center justify-center lg:flex-row">
-            <div className="mt-12 text-center lg:mt-0 lg:text-left lg:col-6">
+            <div className={banner.image_enable ? "mt-12 text-center lg:mt-0 lg:text-left lg:col-6" : "mt-12 text-center lg:mt-0 lg:text-left lg:col-12"}>
               <div className="banner-title">
-                Mint Now
+                {markdownify(banner.title, "h1")}
+                {markdownify(banner.title_small, "span")}
               </div>
-              <div>Counter: <div id="mint-counter"/></div>
-              <button className="btn btn-primary mt-6" id="mint-button-container">MINT NOW</button>
+              {markdownify(banner.content, "p", "mt-4")}
+              {banner.button.enable && (
+                <Link
+                  className="btn btn-primary mt-6"
+                  href={banner.button.link}
+                  rel={banner.button.rel}
+                >
+                  {banner.button.label}
+                </Link>
+              )}
             </div>
+            {banner.image_enable && (
+              <div className="col-9 lg:col-6">
+                <ImageFallback
+                  className="mx-auto object-contain"
+                  src={banner.image}
+                  width={548}
+                  height={443}
+                  priority={true}
+                  alt="Banner Image"
+                />
+              </div>
+            )}
           </div>
         </div>
       </section>
